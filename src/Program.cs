9e14891f5baa993b34cf5ed0f266bc88b3cc959e
neko3cs <minaton.0722.net@gmail.com>
2024-08-16 15:48:56 +0900

    PNG変換処理実装

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cube.Pdf.Ghostscript;
+
+const string usage = "Usage: convertpdf2png <PdfFilePath> <OutputDirPath>";
+if (args.Length != 2)
+{
+    Console.WriteLine(usage);
+    return;
+}
+
+var pdfFilePath = args[0];
+var outputDirPath = args[1];
+
+if (!System.IO.File.Exists(pdfFilePath))
+{
+    Console.WriteLine("Pdf file does not exist.");
+    return;
+}
+if (!pdfFilePath.EndsWith(".pdf"))
+{
+    Console.WriteLine(usage);
+    return;
+}
+if (!System.IO.Directory.Exists(outputDirPath))
+{
+    Console.WriteLine("Output directory does not exist.");
+    return;
+}
+
+var converter = new ImageConverter(Format.Png)
+{
+    Paper = Paper.Auto,
+    Orientation = Orientation.Auto,
+    Resolution = 600,
+};
+
+var pngFilePath = Path.ChangeExtension(
+    Path.Join(
+        outputDirPath,
+        Path.GetFileName(pdfFilePath)),
+    ".png");
+converter.Invoke(pdfFilePath, pngFilePath);
